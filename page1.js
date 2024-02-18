dab30121915f5f9db61ddf96ab21d30d8b4bb59f
Ripon chandra saha <71765905+riponchandrasaha@users.noreply.github.com>
2024-02-19 01:09:55 +0600

    Update page1.js

---
+++ page1.js
@@ -0,0 +1,8 @@
+function play(){
+
+    const Pageone = document.getElementById('page1');
+    //console.log(Pageone);
+    Pageone.classList.add('hidden');
+    const  Pagetwo =document.getElementById('page2');
+    Pagetwo.classList.remove('hidden');
+}
